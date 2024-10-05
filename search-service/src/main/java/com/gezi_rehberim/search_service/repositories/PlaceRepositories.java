93edf7fdc139d12f710923d2da54a1b0f44167f0
OĞUZHAN SEÇGEL <osecgel@hotmail.com>
2024-10-05 20:27:11 +0300

    mvn centralden yeni exception paketi bağımlılığı eklendi. Dockerfile dosyaları oluşturuldu.(Userservice ve searchservice hariç)

--- search-service/src/main/java/com/gezi_rehberim/search_service/repositories/PlaceRepositories.java
+++ search-service/src/main/java/com/gezi_rehberim/search_service/repositories/PlaceRepositories.java
@@ -3,9 +3,12 @@ package com.gezi_rehberim.search_service.repositories;
 import com.gezi_rehberim.search_service.document.Place;
 import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface PlaceRepositories extends ElasticsearchRepository<Place,Integer> {
 
     @Query("{\"wildcard\": {\"name\": \"*?0*\"}}")
