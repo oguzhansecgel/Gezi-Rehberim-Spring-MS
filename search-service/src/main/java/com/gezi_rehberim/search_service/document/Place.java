f838c88d5c656ca5f69aefdd495ddb5db8025a8f
OĞUZHAN SEÇGEL <osecgel@hotmail.com>
2024-10-07 00:31:44 +0300

    Searchservice içinde docker üzerinde ayağa kaldırıldı.

--- search-service/src/main/java/com/gezi_rehberim/search_service/document/Place.java
+++ search-service/src/main/java/com/gezi_rehberim/search_service/document/Place.java
@@ -14,7 +14,7 @@ import org.springframework.data.elasticsearch.annotations.Document;
 @NoArgsConstructor
 public class Place {
     @Id
-    private int id;
+    private String id;
     private String name;
     private String description;
     private String address;
--- search-service/src/main/java/com/gezi_rehberim/search_service/repositories/PlaceRepositories.java
+++ search-service/src/main/java/com/gezi_rehberim/search_service/repositories/PlaceRepositories.java
@@ -3,13 +3,12 @@ package com.gezi_rehberim.search_service.repositories;
 import com.gezi_rehberim.search_service.document.Place;
 import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@EnableElasticsearchRepositories
-public interface PlaceRepositories extends ElasticsearchRepository<Place,Integer> {
+@Repository
+public interface PlaceRepositories extends ElasticsearchRepository<Place,String> {
 
     @Query("{\"wildcard\": {\"name\": \"*?0*\"}}")
     List<Place> searchByName(String name);
